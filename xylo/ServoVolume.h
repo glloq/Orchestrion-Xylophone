643b2b356f6f598f680ac273bb29719ac4964c56
glloq <glloq.nz@gmail.com>
2023-03-31 23:41:14 +0200

    Update ServoVolume.h

--- xylo/ServoVolume.h
+++ xylo/ServoVolume.h
@@ -1,4 +1,9 @@
 /***********************************************************************************************************
+---------------------------------------------------------------------------------------------------------
+------------------------    Ochestrion Project  : Xolophone/Glokenspiel      ---------------------------- 
+-----------------------------------   SERVOVOLUME.H    --------------------------------------------------
+_________________________________________________________________________________________________________
+
 classe pour la gestion du volume et du vibrato avec un servo
 utilise des messages midi comme controles
 permet :
@@ -10,29 +15,33 @@ modifie l'angle en fct du volume  =   void setVolume(int volume);
 Gere le fonctionnement du vibrato =   void setVibrato(bool enabled, float frequency);
 
 
-
-les differents parametres et reglages du systeme pour ce servo est dans settings.h
+les differents parametres et reglages du systeme pour ce servo sont dans settings.h
 ***********************************************************************************************************/
 #ifndef ServoVolume_H
 #define ServoVolume_H
-#include "settings.h" 
+#include "settings.h"
+#include <Servo.h>
+#include "Ticker.h"
 
 class ServoVolume {
   public:
     ServoVolume(int pin);
     void setVolume(int volume);
-    void setVibrato(bool enabled, float frequency=0); // gere l'ajout de la gestion du vibrato avec un servo 
-    void updateVibrato(); // fonction qui gerer la deplacement du servo dans le temps
-
+    void setVibrato(bool enabled, float frequency=0);
+    static void _vibratoTickerCallback();
+    void update();
+    
   private:
+    void updateVibrato();
     int _pin;
-    int _volume;// volume actuel c'est a dire l'angle du servo actuel
+    int _volume;
     bool _vibratoEnabled;
     float _vibratoFrequency;
-    unsigned long _previousMillis;
     int _currentAngle;
-    int angleToPulseWidth(int angle);
-    void writeServo(int angle);
-    void initTimer(); // timer pour l'interuption avec Timer3
+    Servo _servo;
+    Ticker _vibratoTicker;
 };
+
 #endif // ServoVolume_H
+
+
