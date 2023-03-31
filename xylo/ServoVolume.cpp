4547da56678961bba18efb64af02f0a803dfdbce
glloq <glloq.nz@gmail.com>
2023-03-31 23:41:41 +0200

    Update ServoVolume.cpp

--- xylo/ServoVolume.cpp
+++ xylo/ServoVolume.cpp
@@ -1,79 +1,74 @@
+/***********************************************************************************************************
+---------------------------------------------------------------------------------------------------------
+------------------------    Ochestrion Project  : Xolophone/Glokenspiel      ---------------------------- 
+-----------------------------------   SERVOVOLUME.CPP  --------------------------------------------------
+_________________________________________________________________________________________________________
+
+classe pour la gestion du volume et du vibrato avec un servo
+
+les differents parametres et reglages du systeme pour ce servo sont dans settings.h
+***********************************************************************************************************/
 #include "ServoVolume.h"
 #include <Arduino.h>
-#include "settings.h" 
+#include "settings.h"
 
-ServoVolume* servoVolumeInstance;
+static  ServoVolume* ServoVolumeInstance;
 
-ServoVolume::ServoVolume(int pin) : _pin(pin), _volume(0), _vibratoEnabled(false), _vibratoFrequency(0), _previousMillis(0), _currentAngle(0) {
-  pinMode(_pin, OUTPUT);
-  initTimer();
-  servoVolumeInstance = this;
+ServoVolume::ServoVolume(int pin) : _pin(pin), _volume(0), _vibratoEnabled(false), _vibratoFrequency(0), _currentAngle(0), _vibratoTicker(_vibratoTickerCallback, 10, 0, MILLIS) {
+  _servo.attach(_pin);
+
+  if(DEBUG_SEROVOLUME){
+      Serial.println("ServoVolume initialisé"); 
+  }  
+  ServoVolumeInstance = this;
+
 }
 
 void ServoVolume::setVolume(int volume) {
- _volume = map(volume, 0, 127, SERVO_VOLUME_MIN_ANGLE, SERVO_VOLUME_MAX_ANGLE);
-  writeServo(_volume);
+  _volume = map(volume, 0, 127, SERVO_VOLUME_MIN_ANGLE, SERVO_VOLUME_MAX_ANGLE);
+  _servo.write(_volume);
 }
 
 void ServoVolume::setVibrato(bool enabled, float frequency) {
   _vibratoEnabled = enabled;
-  
+  if(DEBUG_SEROVOLUME){
+      Serial.print("ServoVolume Vibrato ="); 
+      Serial.println(_vibratoEnabled); 
+  }      
   if (frequency >= 0) {
     float mappedFrequency = map(frequency, 0, 127, SERVO_VOLUME_MIN_FREQUENCY_VIBRATO, SERVO_VOLUME_MAX_FREQUENCY_VIBRATO);
     _vibratoFrequency = constrain(mappedFrequency, SERVO_VOLUME_MIN_FREQUENCY_VIBRATO, SERVO_VOLUME_MAX_FREQUENCY_VIBRATO);
+  }
+
+  if (_vibratoEnabled) {
+    _vibratoTicker.start();
+  } else {
+   _vibratoTicker.pause();
   }
 }
 
 void ServoVolume::updateVibrato() {
   if (_vibratoEnabled) {
-    unsigned long currentMillis = millis();
-    float timePassed = (currentMillis - _previousMillis) / 1000.0;
     float vibratoRange = _volume - SERVO_VOLUME_MIN_ANGLE_VIBRATO;
-    float vibratoPosition = fmod(timePassed * _vibratoFrequency * 2.0, 2.0);
+    float vibratoPosition = fmod(millis() * _vibratoFrequency * 2.0 / 1000.0, 2.0);
 
     float angleVariation;
+    float vibratoRangeTimesPosition = vibratoRange * vibratoPosition;
     if (vibratoPosition < 1.0) {
-      angleVariation = vibratoRange * vibratoPosition;
+      angleVariation = vibratoRangeTimesPosition;
     } else {
       angleVariation = vibratoRange * (2.0 - vibratoPosition);
     }
     _currentAngle = SERVO_VOLUME_MIN_ANGLE_VIBRATO + angleVariation;
-    writeServo(_currentAngle);
+    _servo.write(_currentAngle);
   }
 }
 
-
-
-int ServoVolume::angleToPulseWidth(int angle) {
-  int pulseWidth = map(angle, 0, 180, SERVO_MIN_PULSE_WIDTH, SERVO_MAX_PULSE_WIDTH);
-  return pulseWidth;
+void ServoVolume::_vibratoTickerCallback() {
+ if (ServoVolumeInstance) {
+    ServoVolumeInstance->updateVibrato();
+  }
 }
-
-void ServoVolume::writeServo(int angle) {
-  int pulseWidth = angleToPulseWidth(angle);
-  digitalWrite(_pin, HIGH);
-  delayMicroseconds(pulseWidth);
-  digitalWrite(_pin, LOW);
+void ServoVolume::update() {
+  _vibratoTicker.update();
 }
-
-void ServoVolume::initTimer() {
-  noInterrupts(); // Désactive les interruptions
-  TCCR3A = 0; // Met à zéro TCCR3A
-  TCCR3B = 0; // Met à zéro TCCR3B
-  TCNT3  = 0; // Initialise le compteur à zéro
-
-  // Configurez la fréquence de l'interruption TIMER3_COMPA_vect
-  uint32_t compareValue = (F_CPU / (SERVO_FREQUENCY * 8)) - 1;
-
-  OCR3A = compareValue; // Configure la valeur de comparaison
-  TCCR3B |= (1 << WGM32); // Mode CTC
-  TCCR3B |= (1 << CS31); // Pré-diviseur 8
-  TIMSK3 |= (1 << OCIE3A); // Active l'interruption TIMER3_COMPA_vect
-  interrupts(); // Active les interruptions
-}
-
-
-// Fonction d'interruption pour Timer3
-ISR(TIMER3_COMPA_vect) {
-  // Gérer le vibrato
-  servoVolumeInstance->updateVibrato();}
